d5abb175f67945b38ae296ba30b997695a3bd6af
Your Name <you@example.com>
2026-06-27 15:51:25 +0530

    Create analyze_string function

--- Vowels .py
+++ Vowels .py
@@ -1,15 +1,15 @@
 def analyze_string(text):
-    vowels = "aeiou AEIOU"
+    vowels = "aeiouAEIOU"
     vowels_count = 0
 
     #loop for counting vowels
     for ch in text:
         if ch in vowels:
-            vowel_count += 1
+            vowels_count += 1
     print("original string :",text)
     print("Total Character :",len(text))
-    print("total vowels :",vowel_count)
+    print("total vowels :",vowels_count)
 
     #main program
-    String = input("Enter a string:")
-    analyze_string(String)
+String = input("Enter a string:")
+analyze_string(String)
